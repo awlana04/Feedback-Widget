a49995e9edb3dc356f3819ef30ecf67a1d198101
Awlana Costa <cawlana040@gmail.com>
2022-05-04 19:08:13 -0300

    tests with jest

---
+++ server/src/usecases/submit-feedback-use-case.spec.ts
@@ -0,0 +1,69 @@
+import { SubmitFeedbackUseCase } from "./submit-feedback-use-case";
+
+const createFeedbackSpy = jest.fn();
+const sendMailSpy = jest.fn();
+
+describe("Submit feedback", () => {
+  it("should be able to submit a feedback", async () => {
+    const submitFeedback = new SubmitFeedbackUseCase(
+      { create: createFeedbackSpy },
+      { sendMail: sendMailSpy }
+    );
+
+    await expect(
+      submitFeedback.execute({
+        type: "BUG",
+        comment: "example comment",
+        screenshot: "data:image/png;base64.3203kdado2k2mekmo2k302o-adlaçdĺ",
+      })
+    ).resolves.not.toThrow();
+
+    expect(createFeedbackSpy).toHaveBeenCalled();
+    expect(sendMailSpy).toHaveBeenCalled();
+  });
+
+  it("should not be able to submit a feedback without type", async () => {
+    const submitFeedback = new SubmitFeedbackUseCase(
+      { create: async () => {} },
+      { sendMail: async () => {} }
+    );
+
+    await expect(
+      submitFeedback.execute({
+        type: "",
+        comment: "example comment",
+        screenshot: "data:image/png;base64.3203kdado2k2mekmo2k302o-adlaçdĺ",
+      })
+    ).rejects.toThrow();
+  });
+
+  it("should not be able to submit a feedback without a comment", async () => {
+    const submitFeedback = new SubmitFeedbackUseCase(
+      { create: async () => {} },
+      { sendMail: async () => {} }
+    );
+
+    await expect(
+      submitFeedback.execute({
+        type: "BUG",
+        comment: "",
+        screenshot: "data:image/png;base64.3203kdado2k2mekmo2k302o-adlaçdĺ",
+      })
+    ).rejects.toThrow();
+  });
+
+  it("should not be able to submit a feedback with an invalid screenshot", async () => {
+    const submitFeedback = new SubmitFeedbackUseCase(
+      { create: async () => {} },
+      { sendMail: async () => {} }
+    );
+
+    await expect(
+      submitFeedback.execute({
+        type: "BUG",
+        comment: "example comment",
+        screenshot: "3203kdado2k2mekmo2k302o-adlaçdĺ",
+      })
+    ).rejects.toThrow();
+  });
+});
